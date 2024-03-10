442ae1a0ee1f7ff283f661d98331608366cbdc26
IsabelCoronaGuevara <77768954+IsabelCoronaGuevara@users.noreply.github.com>
2024-03-10 17:14:22 -0600

    Update aPCE.py

--- aPCE.py
+++ aPCE.py
@@ -81,7 +81,16 @@ class aPCE(object):
         
         return s
 
-    def Inner_prod(self, coeff1, coeff2, dat, a1, b1):
+    def Inner_prod_MC(self, coeff1, coeff2, dat):
+        """
+        Approximates the integral of the product of 2 polynomials.
+        """
+
+        s = np.mean(self.Pol_eval(coeff1, dat)*self.Pol_eval(coeff2, dat))
+        
+        return s
+    
+    def Inner_prod_Quad(self, coeff1, coeff2, dat, a1, b1):
         """
         Approximates the integral of the product of 2 polynomials.
         """
@@ -129,7 +138,7 @@ class aPCE(object):
             val += P[pol_d][c_idx[i][0]]*P[pol_d][c_idx[i][1]]*np.mean(dat**(np.sum(c_idx[i])))
         return np.sqrt(val)
     
-    def Create_Orthonormal_Polynomials(self, p, domain):
+    def Create_Orthonormal_Polynomials_Quad(self, p, domain):
         """
         
         """
@@ -169,11 +178,62 @@ class aPCE(object):
             P_temp_norm = np.zeros((p+1, p+1))
             for i in range(p+1):
                 #P_temp_norm[i,:] = P_temp[i,:]/self.Norm(P_temp, self.X[:,j], i)
-                P_temp_norm[i,:] = P_temp[i,:]/(np.sqrt(self.Inner_prod(P_temp[i,:],P_temp[i,:],self.X[:,j], a1, b1)))
+                P_temp_norm[i,:] = P_temp[i,:]/(np.sqrt(self.Inner_prod_Quad(P_temp[i,:],P_temp[i,:],self.X[:,j], a1, b1)))
             # Adding Matrix with Polynomial Coefficients to P
             P.append(P_temp_norm)
             
         return P
+    
+    def Create_Orthonormal_Polynomials(self, p):
+        """
+        
+        """
+        P = []
+                    
+        for j in range(self.d):
+            # Creating Orthogonal Polynomials of size n_quad
+            P_temp = np.zeros((p+1, p+1))
+
+            P_temp[0,0] = 1
+
+            mu = np.zeros(2*p) 
+            for i in range(2*p): 
+                
+                mu[i] = np.mean(self.X[:,j]**i)
+
+            mu_mat = np.zeros((p, p+1))
+            
+            for i in range(p):
+                mu_mat[i,:] = mu[i:(p+1+i)]
+
+            for i in range(1, p+1):
+                A = np.zeros((i+1, i+1))
+                A[-1,-1] = 1
+                A[0:i,:] = mu_mat[0:i, 0:i+1]
+                b = A[-1]
+                a = np.zeros(p+1)
+                a[0:i+1] = np.linalg.inv(A)@b
+                P_temp[i, :] = a.transpose()
+
+            # Normalizing Polynomials
+            P_temp_norm = np.zeros((p+1, p+1))
+            for i in range(p+1):
+                #P_temp_norm[i,:] = P_temp[i,:]/self.Norm(P_temp, self.X[:,j], i)
+                P_temp_norm[i,:] = P_temp[i,:]/(np.sqrt(self.Inner_prod_MC(P_temp[i,:],P_temp[i,:],self.X[:,j])))
+            # Adding Matrix with Polynomial Coefficients to P
+            P.append(P_temp_norm)
+            
+        return P
+    
+    def P_ip1(self, P_i, P_im1, alpha, beta):
+        return np.roll(P_i,1) - alpha*P_i - beta*P_im1
+
+    def alpha(self, P_i, X):
+        return np.mean(X*self.Pol_eval(P_i, X)*self.Pol_eval(P_i, X))/np.mean(self.Pol_eval(P_i, X)*self.Pol_eval(P_i, X))
+
+    def beta(self, P_i, P_im1, X):
+        return np.mean(self.Pol_eval(P_i, X)*self.Pol_eval(P_i, X))/np.mean(self.Pol_eval(P_im1, X)*self.Pol_eval(P_im1, X))
+
     
     def P_ip1(self, P_i, P_im1, alpha, beta):
         return np.roll(P_i,1) - alpha*P_i - beta*P_im1
