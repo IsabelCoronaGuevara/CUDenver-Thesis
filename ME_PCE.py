2ae459ea4f2a68e73f696eff66c94449684bb4cf
IsabelCoronaGuevara <77768954+IsabelCoronaGuevara@users.noreply.github.com>
2024-04-05 12:53:18 -0600

    Update ME_PCE.py

--- ME_PCE.py
+++ ME_PCE.py
@@ -22,30 +22,33 @@ class ME_PCE(BaseEstimator):
     
     """
     
-    def __init__(self, PCE_method, d, p, B_init, fun, alg_mod, X_pol = None, sparse = False, theta1 = 0.001, theta2 = 0.01, alpha = 0.5, size_restriction = 25, arg1 = 0.01, arg2 = 0.0001, arg3 = 0.01, arg4= 0.0001, sigma_vals = None, mu_vals = None):
+    def __init__(self, PCE_method, d, p, B_init, fun, alg_mod, data_fun, N_t, N_p, theta1 = 0.00001, theta2 = 0.00001, alpha = 0.5, arg1 = 0.01, arg2 = 0.0001, arg3 = 0.01, arg4= 0.0001, sigma_vals = None, mu_vals = None, n_iter = 2):
         """
         PCE_method: 'aPCE' or 'aPCE_Stieltjes' or 'PCE_Legendre' or 'PCE_Hermite'
+        alg_mod: AFVB_PCE or VRVM_PCE
+        data_fun: function to generate data
 
         """
         
-        self.X_pol = X_pol
+        self.data_fun = data_fun
         self.B_init = B_init
         self.fun = fun
         self.alg_mod = alg_mod
         self.PCE_method = PCE_method
         self.p = p
         self.d = d
-        self.sparse = sparse
         self.theta1 = theta1
         self.theta2 = theta2
         self.alpha = alpha
-        self.size_restriction = size_restriction
         self.arg1 = arg1
         self.arg2 = arg2
         self.arg3 = arg3
         self.arg4 = arg4
         self.sigma_vals = sigma_vals
         self.mu_vals = mu_vals
+        self.n_iter = n_iter
+        self.N_p = N_p
+        self.N_t = N_t
         
     def multivariate_pce_index(self, d, max_deg):
         """
@@ -93,7 +96,7 @@ class ME_PCE(BaseEstimator):
 
         return B_k_new
     
-    def split_domain(self, X_train, X_pol, theta1, theta2, alpha, size_restriction = 25, iter_num = 10):
+    def split_domain(self, N_t, N_p, theta1, theta2, alpha, iter_num):
         """
         Inputs:
         
@@ -114,31 +117,25 @@ class ME_PCE(BaseEstimator):
         for j in range(iter_num):
             B_k = []
                 
-            for k in range(len(B)):
-                size_reached = False    
+            for k in range(len(B)):   
                 if (self.PCE_method == 'aPCE'):
-                    X_p = self.split_data(X_pol, np.array(B[k]))
+                    X_p = self.data_fun(N_p, self.d, B[k])
                     mod = aPCE(X_p, self.p)
                     P = mod.Create_Orthonormal_Polynomials(self.p)  ######
                 elif (self.PCE_method == 'aPCE_Stieltjes'):
-                    X_p = self.split_data(X_pol, np.array(B[k]))
+                    X_p = self.data_fun(N_p, self.d, B[k])
                     mod = aPCE(X_p, self.p)
                     P = mod.Create_Orthonormal_Polynomials_Stieltjes(self.p)
                 else:
                     mod = None
                     P = None
                     
-                X_t = self.split_data(X_train, np.array(B[k]))
+                X_t = self.data_fun(N_t, self.d, B[k])
                 Y_t = self.fun(X_t)
 
                 model = self.alg_mod(self.PCE_method, self.d, self.p, B[k], mod, P, self.arg1, self.arg2, self.arg3, self.arg4, sigma_vals = self.sigma_vals, mu_vals = self.mu_vals).fit(X_t, Y_t.reshape(X_t.shape[0]))
 
-                if self.sparse is True:
-                    a_vec = np.zeros((self.n,1))
-                    a_vec[model.active_cols] = np.c_[model.a_full[model.active_cols]]
-
-                elif self.sparse is False:
-                    a_vec = model.a_full
+                a_vec = model.a_full
 
                 # For each random element k, calculate eta_k and J_k
                 N_p_1 = int(math.factorial(self.d + self.p-1)/(math.factorial(self.d)*math.factorial(self.p-1)))
@@ -207,15 +204,7 @@ class ME_PCE(BaseEstimator):
                                 B_new[t].append(B_temp[i][0])
                                 t += 1
                     #print(B_new)
-                    for j in range(len(B_new)):
-                        if (self.split_data(X_train, np.array(B_new[j])).shape[0] < self.size_restriction):
-                            size_reached = True
-                        
-                    if size_reached is False:
-                        B_k = B_k+B_new
-                    else:
-                        B_new = B[k]
-                        B_k = B_k + [B_new]
+                    B_k = B_k+B_new
                     
                 else:
                     #print('Not Splitting', 'time =', j, 'k =', k)
@@ -227,14 +216,6 @@ class ME_PCE(BaseEstimator):
             # stop the iteration if no splitting is to occur next
             if B == B_k:
                 break
-                
-            #for j in range(len(B)):
-                #if (self.split_data(X_train, np.array(B_k[j])).shape[0] < self.size_restriction):
-                    #size_reached = True
-            
-            #if size_reached is True:
-                #print('not enough data in region, stop algorithm')
-                #break
 
             B = B_k.copy()
             
@@ -246,8 +227,6 @@ class ME_PCE(BaseEstimator):
         """
          
         self.idx = self.multivariate_pce_index(self.d, self.p)
-        self.N = X_train.shape[0] # sample size
-        self.d = X_train.shape[1] # number of parameters
         self.n = int(math.factorial(self.d + self.p)/(math.factorial(self.d)*math.factorial(self.p)))
         
         model_local = []
@@ -258,22 +237,26 @@ class ME_PCE(BaseEstimator):
         a_local = []
         a_local_full = []
         active_cols_local = []
+        Jk_i = []
         Jk = []
         n_star_local = []
-
-        B = self.split_domain(X_train, self.X_pol, self.theta1, self.theta2, self.alpha, self.size_restriction)
+        
+        if self.n_iter == 0:
+            B = self.B_init
+        else:
+            B = self.split_domain(self.N_t, self.N_p, self.theta1, self.theta2, self.alpha, self.n_iter)
 
         for k in range(len(B)):
             
             if (self.PCE_method == 'aPCE'):    
-                X_p = self.split_data(self.X_pol, B[k])
+                X_p = self.data_fun(self.N_p, self.d, B[k])
                 mod = aPCE(X_p, self.p)
                 mod_local.append(mod)
                 P = mod.Create_Orthonormal_Polynomials(self.p)
                 P_local.append(P)
                 
             elif (self.PCE_method == 'aPCE_Stieltjes'):    
-                X_p = self.split_data(self.X_pol, B[k])
+                X_p = self.data_fun(self.N_p, self.d, B[k])
                 mod = aPCE(X_p, self.p)
                 mod_local.append(mod)
                 P = mod.Create_Orthonormal_Polynomials_Stieltjes(self.p)
@@ -281,26 +264,34 @@ class ME_PCE(BaseEstimator):
             else:
                 mod = None
                 P = None
-                
-            X_t = self.split_data(X_train, B[k])
+         
+            if self.n_iter == 0:
+                X_t = self.split_data(X_train, B[k])
+            else:
+                X_t = self.data_fun(self.N_t, self.d, B[k])
             #print(X_t.shape[0], 'k =', k)
             Y_t = self.fun(X_t)
 
             model = self.alg_mod(self.PCE_method, self.d, self.p, B[k], mod, P, self.arg1, self.arg2, self.arg3, self.arg4, sigma_vals = self.sigma_vals, mu_vals = self.mu_vals).fit(X_t, Y_t.reshape(X_t.shape[0]))
             
-            J_k = 1
-            for i in range(self.d):
-                Bk_trans = self.map_domain_to_negOne_One(B[k], self.B_init)
-                J_k *= (Bk_trans[i][1] - Bk_trans[i][0])/2
-                
-            Jk.append(J_k)
-            model_local.append(model)
-            mean_local.append(float(model.a_hat[0]))
-            v_local.append(np.sum(model.a_hat[1:]**2))
-            a_local.append(model.a_hat)
-            active_cols_local.append(model.active_cols)
-            n_star_local.append(model.n_star)
-            a_local_full.append(model.a_full)
+            if self.n_iter != 0:
+                J_k = 1
+                Jk_i_temp = []
+                for i in range(self.d):
+                    Bk_trans = self.map_domain_to_negOne_One(B[k], self.B_init)
+                    J_k *= (Bk_trans[i][1] - Bk_trans[i][0])/2
+                    Jk_i_temp.append((Bk_trans[i][1] - Bk_trans[i][0])/2)
+
+                Jk_i.append(Jk_i_temp)
+
+                Jk.append(J_k)
+                model_local.append(model)
+                mean_local.append(float(model.a_hat[0]))
+                v_local.append(np.sum(model.a_hat[1:]**2))
+                a_local.append(model.a_hat)
+                active_cols_local.append(model.active_cols)
+                n_star_local.append(model.n_star)
+                a_local_full.append(model.a_full)
 
         self.B_split = B
         self.P_local = P_local
@@ -313,6 +304,7 @@ class ME_PCE(BaseEstimator):
         self.Jk = np.array(Jk)
         self.n_star_local = n_star_local
         self.a_local_full = a_local_full
+        self.Jk_i = np.array(Jk_i)
         
         return self
         
